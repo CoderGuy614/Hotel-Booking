ca9057675d96697adeff19cbbbd6d354cf6a2af9
CoderGuy614 <jlutz.110@gmail.com>
2021-08-18 21:09:15 -0400

    Setup Redux Store and User Reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./booking/Home";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import TopNav from "./components/TopNav";
 
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
